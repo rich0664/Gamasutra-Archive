b76635fc1e039472bc398bf4f02ad91bba80e9a5
rich0664 <rich0664@gmail.com>
2026-04-26 21:20:54 +0000

    does this finally get it running?

--- src/index.ts
+++ src/index.ts
@@ -75,7 +75,8 @@ async function initDbWorker() {
                     serverMode: "full",
                     url: "https://rich0664.github.io/Gamasutra-Archive/Data/gamedeveloper_blogs.sqlite3",
                     requestChunkSize: 4096,
-                },
+                    fileLength: 14942208
+                }as any,
             },
         ],
         workerUrl.toString(),
